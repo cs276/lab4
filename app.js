e1426d66dfe48744f3c1426bd786363cfbf0237a
Doug Lloyd <lloyd@cs50.harvard.edu>
2018-10-11 12:39:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(expressVueMiddleware);
 
 // List galleries
 app.get('/', (req, res) => {
-    let galleries = [];
+    let galleries = ['Gallery A', 'Gallery B', 'Gallery C'];
     res.renderVue('index.vue', {galleries: galleries});
 });
 
